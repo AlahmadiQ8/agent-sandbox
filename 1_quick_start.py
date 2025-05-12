ceeb748e13a11d47d58e3704563d1f017d160d60
Mohammad Mohammad <momohammad@microsoft.com>
2025-05-12 22:15:56 +0300

    init

---
+++ 1_quick_start.py
@@ -0,0 +1,72 @@
+from openai import AsyncAzureOpenAI
+from openai.types.responses import ResponseTextDeltaEvent
+from agents import Agent, InputGuardrail, GuardrailFunctionOutput, Runner, set_default_openai_client
+from pydantic import BaseModel
+from dotenv import load_dotenv
+import asyncio
+import os
+
+load_dotenv()
+
+model_name = "gpt-4.1"
+
+# Create OpenAI client using Azure OpenAI
+openai_client = AsyncAzureOpenAI(
+    api_key=os.getenv("AZURE_OPENAI_API_KEY"),
+    api_version=os.getenv("AZURE_OPENAI_API_VERSION"),
+    azure_endpoint=os.getenv("AZURE_OPENAI_ENDPOINT"),
+)
+
+set_default_openai_client(client=openai_client, use_for_tracing=False)
+
+class HomeworkOutput(BaseModel):
+    is_homework: bool
+    reasoning: str
+
+guardrail_agent = Agent(
+    name="Guardrail check",
+    instructions="Check if the user is asking about homework.",
+    output_type=HomeworkOutput,
+    model=model_name
+)
+
+math_tutor_agent = Agent(
+    name="Math Tutor",
+    handoff_description="Specialist agent for math questions",
+    instructions="You provide help with math problems. Explain your reasoning at each step and include examples",
+    model=model_name
+)
+
+history_tutor_agent = Agent(
+    name="History Tutor",
+    handoff_description="Specialist agent for historical questions",
+    instructions="You provide assistance with historical queries. Explain important events and context clearly.",
+    model=model_name
+)
+
+async def homework_guardrail(ctx, agent, input_data):
+    result = await Runner.run(guardrail_agent, input_data, context=ctx.context)
+    final_output = result.final_output_as(HomeworkOutput)
+    return GuardrailFunctionOutput(
+        output_info=final_output,
+        tripwire_triggered=not final_output.is_homework,
+    )
+
+triage_agent = Agent(
+    name="Triage Agent",
+    instructions="You determine which agent to use based on the user's homework question",
+    handoffs=[history_tutor_agent, math_tutor_agent],
+    input_guardrails=[
+        InputGuardrail(guardrail_function=homework_guardrail),
+    ],
+    model=model_name
+)
+
+async def main():
+    result = Runner.run_streamed(triage_agent, "who was the first president of the united states?")
+    async for event in result.stream_events():
+        if event.type == "raw_response_event" and isinstance(event.data, ResponseTextDeltaEvent):
+            print(event.data.delta, end="", flush=True)
+
+if __name__ == "__main__":
+    asyncio.run(main())
